34f06b87126f90baf7d19b4089ce5ed06e76240d
Holly <holly@Holly.hu.fs.wireless.unca>
2012-09-05 16:43:02 -0400

    first Euler problem

--- hello.cpp
+++ hello.cpp
@@ -4,4 +4,19 @@ using namespace std;
 int main ()
 {
 	cout << "Hello, World!" << endl;
+
+	//adds all multiples of 3 and 5 below 1000
+	int result = 0;
+	for(int i = 0; i < 1000; i++){
+		if(i % 3 == 0 && i % 5 == 0 ) {
+			result = result + i;
+		}
+		else if(i % 5 == 0 ) {
+			result = result + i;
+		}
+		else if(i % 3 == 0 ) {
+			result = result + i;
+		}
+	}
+	cout << result << endl;
 }
